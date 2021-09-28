77cabbae0e7a0a4a3b22f1d8fa4645460bede06c
mikohei <mikohei@amazon.co.jp>
2021-09-28 10:11:50 +0900

    [ADD] chap1

---
+++ src/chap1/Questions/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+
+
+namespace Questions
+{
+    public class Questions
+    {
+        public static void Q1()
+        {
+            Console.WriteLine("Hello World");
+        }
+
+        public static void Q2()
+        {
+            int x = 11;
+            Console.WriteLine($"x={x}");
+        }
+
+        public static void Q3()
+        {
+            int x = 13;
+            int y = 17;
+            Console.WriteLine($"x={x},y={y}");
+        }
+
+        public static void Q4()
+        {
+            int x = 13;
+            int y = 17;
+            x = x + y;
+            Console.WriteLine(x);
+        }
+
+        public static void Q5()
+        {
+            int x = 13;
+            int y = 17;
+            Console.WriteLine(x + y);
+        }
+
+        public static void Q6()
+        {
+            int x = 7;
+            x *= 3;
+            Console.WriteLine(x);
+            x /= 2;
+            Console.WriteLine(x);
+        }
+
+        public static void Q7()
+        {
+            int x = 7;
+            int y = 3;
+            Console.WriteLine($"x={x},y={y}");
+            (x, y) = (y, x);
+            Console.WriteLine($"x={x},y={y}");
+        }
+
+        public static void Q8()
+        {
+            int x = 19;
+            int y = 23;
+            int z = x * y;
+            Console.WriteLine(z);
+        }
+
+        public static void Q9()
+        {
+            int x = 3;
+            for (int i = 2; i <= 4; i++)
+            {
+                Console.WriteLine(x * i);
+            }
+        }
+
+        public static void Q10()
+        {
+            int x = 3;
+            for (int i = 1; i <= 3; i++)
+            {
+                Console.WriteLine(Math.Pow(x, i));
+            }
+        }
+
+        public static void Q11()
+        {
+            int x = 10;
+            for (int i = 1; i < x; i++)
+            {
+                int div = x / i;
+                int mod = x % i;
+                Console.WriteLine($"div={div}, mod={mod}");
+            }
+        }
+
+        public static void Q12()
+        {
+            int x = 10;
+            Console.WriteLine(x++);
+            Console.WriteLine(x--);
+            Console.WriteLine(--x);
+            Console.WriteLine(++x);
+        }
+
+        static int Main()
+        {
+            return 0;
+        }
+    }
+}
---
+++ src/chap1/chap1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace chap1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Questions.Questions.Q1();
+            Console.WriteLine("---");
+            Questions.Questions.Q2();
+            Console.WriteLine("---");
+            Questions.Questions.Q3();
+            Console.WriteLine("---");
+            Questions.Questions.Q4();
+            Console.WriteLine("---");
+            Questions.Questions.Q5();
+            Console.WriteLine("---");
+            Questions.Questions.Q6();
+            Console.WriteLine("---");
+            Questions.Questions.Q7();
+            Console.WriteLine("---");
+            Questions.Questions.Q8();
+            Console.WriteLine("---");
+            Questions.Questions.Q9();
+            Console.WriteLine("---");
+            Questions.Questions.Q10();
+            Console.WriteLine("---");
+            Questions.Questions.Q11();
+            Console.WriteLine("---");
+            Questions.Questions.Q12();
+            Console.WriteLine("---");
+        }
+    }
+}
